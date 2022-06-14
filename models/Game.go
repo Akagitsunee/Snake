b18482e803ad76566f04d8f441bc0ceb5ffc5b66
Ultrayano <ultrayanoo@gmail.com>
2022-06-14 20:36:08 +0200

    Update readme and extract logic to functions into goroutines

--- models/Game.go
+++ models/Game.go
@@ -3,11 +3,10 @@ package models
 import (
 	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
-	"image/color"
-	"math/rand"
-
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"image/color"
+	"math/rand"
 )
 
 type Game struct {
@@ -39,9 +38,9 @@ const (
 	dirUp
 )
 
-func (g *Game) collidesWithApple(snakeBody []Position) bool {
-	return snakeBody[0].X == g.Apple.X &&
-		snakeBody[0].Y == g.Apple.Y
+func (g *Game) collidesWithApple(snakeBody *[]Position) bool {
+	return (*snakeBody)[0].X == g.Apple.X &&
+		(*snakeBody)[0].Y == g.Apple.Y
 }
 
 func (g *Game) collidesWithSelf(snakeBody *[]Position) bool {
@@ -100,83 +99,62 @@ func (g *Game) reset() {
 	g.MoveDirectionP2 = dirNone
 }
 
+func (g *Game) isKeyJustPressed(left ebiten.Key, right ebiten.Key, up ebiten.Key, down ebiten.Key, moveDirection *int) {
+	if inpututil.IsKeyJustPressed(left) {
+		if *moveDirection != dirRight {
+			*moveDirection = dirLeft
+		}
+	} else if inpututil.IsKeyJustPressed(right) {
+		if *moveDirection != dirLeft {
+			*moveDirection = dirRight
+		}
+	} else if inpututil.IsKeyJustPressed(down) {
+		if *moveDirection != dirUp {
+			*moveDirection = dirDown
+		}
+	} else if inpututil.IsKeyJustPressed(up) {
+		if *moveDirection != dirDown {
+			*moveDirection = dirUp
+		}
+	}
+}
+
+func (g *Game) snakeMustMove(snake *[]Position, moveDirection *int) {
+	if g.collidesWithWall(snake) || g.collidesWithSelf(snake) || g.collidesWithOtherSnake(snake) {
+		g.reset()
+	}
+
+	if g.collidesWithApple(snake) {
+		g.eatApple(snake)
+	}
+
+	for i := int64(len(*snake)) - 1; i > 0; i-- {
+		(*snake)[i].X = (*snake)[i-1].X
+		(*snake)[i].Y = (*snake)[i-1].Y
+	}
+	switch *moveDirection {
+	case dirLeft:
+		(*snake)[0].X--
+	case dirRight:
+		(*snake)[0].X++
+	case dirDown:
+		(*snake)[0].Y++
+	case dirUp:
+		(*snake)[0].Y--
+	}
+}
+
 func (g *Game) Update() error {
-	if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
-		if g.MoveDirectionP1 != dirRight {
-			g.MoveDirectionP1 = dirLeft
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
-		if g.MoveDirectionP1 != dirLeft {
-			g.MoveDirectionP1 = dirRight
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
-		if g.MoveDirectionP1 != dirUp {
-			g.MoveDirectionP1 = dirDown
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
-		if g.MoveDirectionP1 != dirDown {
-			g.MoveDirectionP1 = dirUp
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyA) {
-		if g.MoveDirectionP2 != dirRight {
-			g.MoveDirectionP2 = dirLeft
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyD) {
-		if g.MoveDirectionP2 != dirLeft {
-			g.MoveDirectionP2 = dirRight
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyS) {
-		if g.MoveDirectionP2 != dirUp {
-			g.MoveDirectionP2 = dirDown
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyW) {
-		if g.MoveDirectionP2 != dirDown {
-			g.MoveDirectionP2 = dirUp
-		}
-	} else if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+	go g.isKeyJustPressed(ebiten.KeyA, ebiten.KeyD, ebiten.KeyW, ebiten.KeyS, &g.MoveDirectionP2)
+	go g.isKeyJustPressed(ebiten.KeyArrowLeft, ebiten.KeyArrowRight, ebiten.KeyArrowUp, ebiten.KeyArrowDown, &g.MoveDirectionP1)
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
 		g.reset()
 	}
 
 	if g.needsToMoveSnake() {
-		if g.collidesWithWall(&g.SnakeBodyP1) || g.collidesWithWall(&g.SnakeBodyP2) || g.collidesWithSelf(&g.SnakeBodyP1) || g.collidesWithSelf(&g.SnakeBodyP2) || g.collidesWithOtherSnake(&g.SnakeBodyP1) || g.collidesWithOtherSnake(&g.SnakeBodyP2) {
-			g.reset()
-		}
-
-		if g.collidesWithApple(g.SnakeBodyP1) {
-			go g.eatApple(&g.SnakeBodyP1)
-		} else if g.collidesWithApple(g.SnakeBodyP2) {
-			g.eatApple(&g.SnakeBodyP2)
-		}
-
-		for i := int64(len(g.SnakeBodyP1)) - 1; i > 0; i-- {
-			g.SnakeBodyP1[i].X = g.SnakeBodyP1[i-1].X
-			g.SnakeBodyP1[i].Y = g.SnakeBodyP1[i-1].Y
-		}
-		switch g.MoveDirectionP1 {
-		case dirLeft:
-			g.SnakeBodyP1[0].X--
-		case dirRight:
-			g.SnakeBodyP1[0].X++
-		case dirDown:
-			g.SnakeBodyP1[0].Y++
-		case dirUp:
-			g.SnakeBodyP1[0].Y--
-		}
-
-		for i := int64(len(g.SnakeBodyP2)) - 1; i > 0; i-- {
-			g.SnakeBodyP2[i].X = g.SnakeBodyP2[i-1].X
-			g.SnakeBodyP2[i].Y = g.SnakeBodyP2[i-1].Y
-		}
-		switch g.MoveDirectionP2 {
-		case dirLeft:
-			g.SnakeBodyP2[0].X--
-		case dirRight:
-			g.SnakeBodyP2[0].X++
-		case dirDown:
-			g.SnakeBodyP2[0].Y++
-		case dirUp:
-			g.SnakeBodyP2[0].Y--
-		}
+		go g.snakeMustMove(&g.SnakeBodyP1, &g.MoveDirectionP1)
+		go g.snakeMustMove(&g.SnakeBodyP2, &g.MoveDirectionP2)
 	}
 
 	g.Timer++
@@ -206,20 +184,22 @@ func (g *Game) eatApple(snakeBody *[]Position) {
 	}
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	for _, v := range g.SnakeBodyP1 {
-		go ebitenutil.DrawRect(screen, float64(v.X*GridSize), float64(v.Y*GridSize), GridSize, GridSize, color.RGBA{0x80, 0xa0, 0xc0, 0xff})
+func (g *Game) drawBody(screen *ebiten.Image, snake *[]Position, rgba color.RGBA) {
+	for _, v := range *snake {
+		ebitenutil.DrawRect(screen, float64(v.X*GridSize), float64(v.Y*GridSize), GridSize, GridSize, rgba)
 	}
-	ebitenutil.DrawRect(screen, float64(g.Apple.X*GridSize), float64(g.Apple.Y*GridSize), GridSize, GridSize, color.RGBA{0xFF, 0x00, 0x00, 0xff})
+}
 
-	if g.MoveDirectionP1 == dirNone {
+func (g *Game) Draw(screen *ebiten.Image) {
+	go g.drawBody(screen, &g.SnakeBodyP1, color.RGBA{G: 0xff, B: 0x79, A: 0xCC})
+	go g.drawBody(screen, &g.SnakeBodyP2, color.RGBA{B: 0xff, A: 0xCC})
+
+	go ebitenutil.DrawRect(screen, float64(g.Apple.X*GridSize), float64(g.Apple.Y*GridSize), GridSize, GridSize, color.RGBA{0xFF, 0x00, 0x00, 0xff})
+
+	if g.MoveDirectionP1 == dirNone && g.MoveDirectionP2 == dirNone {
 		ebitenutil.DebugPrint(screen, fmt.Sprintf("Press up/down/left/right to start"))
 	} else {
 		ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f Level: %d Score: %d Best Score: %d", ebiten.CurrentFPS(), g.Level, g.Score, g.BestScore))
-	}
-
-	for _, v := range g.SnakeBodyP2 {
-		go ebitenutil.DrawRect(screen, float64(v.X*GridSize), float64(v.Y*GridSize), GridSize, GridSize, color.RGBA{0x80, 0xa0, 0xc0, 0xff})
 	}
 }
 
